8f7150b88bdeebe14f65026a00b9bb0368ad541e
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-12 15:50:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_fourth_tier
     :names => {reading_railroad: {"mortgage value"}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}},
   },
   }
-  
+  :railroads[:names][:reading_railroad][0] = "$100" 
   
   
   
