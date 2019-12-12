9203d9a80b9d8775ac754d6cd7f33e5c4392bab0
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-12 15:22:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ end
 def monopoly_with_third_tier
   {:railroads => {
     :pieces => 4,
-    :rent_in_dollars => {key1:, key2:, key3, key4}
+    :rent_in_dollars => {key: "1", key2: "2", key3: "3", key4: "4"}
   },
   }
   
